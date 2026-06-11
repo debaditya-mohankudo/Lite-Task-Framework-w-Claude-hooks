107cf0ad022f777be67208f91451ab3c9fcaeb24
Debaditya <debadityamohankudo@gmail.com>
2026-06-11 06:59:14 +0530

    fix(gates): elevate name_arg_check log to INFO for visibility

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -48,6 +48,7 @@ DOMAIN_MAP: dict[str, tuple[str, list[str]]] = {
                                   "set_active", "clear_active", "pop_active", "finish",
                                   "log_event", "history", "add_decision",
                                   "relate", "unrelate", "neighbors"]),
+    "code_rag": ("tools.code_rag", ["query", "smart_search", "index_files"]),
 }
 
 
